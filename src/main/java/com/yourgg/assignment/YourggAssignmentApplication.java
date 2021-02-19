42ac1dc827b279288095df15ce30cc2ae49fb315
Ramen6315 <ramen6315@gmail.com>
2021-02-19 20:30:58 +0900

    feat: yourgg과제 프로젝트 생성

---
+++ src/main/java/com/yourgg/assignment/YourggAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.yourgg.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourggAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourggAssignmentApplication.class, args);
+    }
+
+}
