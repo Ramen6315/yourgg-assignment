0c4f4c3fc07d7ae252cae39dba86d502ac3fe6d5
Ramen6315 <ramen6315@gmail.com>
2021-02-21 14:59:48 +0900

    refactor: SummonerController @Controller -> @RestController 변경

--- src/main/java/com/yourgg/assignment/summoner/SummonerController.java
+++ src/main/java/com/yourgg/assignment/summoner/SummonerController.java
@@ -1,13 +1,13 @@
 package com.yourgg.assignment.summoner;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.yourgg.assignment.service.SummonerService;
 import com.yourgg.assignment.service.dto.SummonerDto;
 
-@Controller
+@RestController
 public class SummonerController {
 
     private SummonerService summonerService;
